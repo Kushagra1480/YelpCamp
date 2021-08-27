7ac1909b190f0ac5979f12e2d8eff19e930d0b46
Kushagra Kartik <60288916+Kushagra1480@users.noreply.github.com>
2021-08-27 11:31:42 +0400

    Add files via upload

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground ({
+            author: '6123eb7b2dc6fa19d42cd962',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title : `${sample(descriptors)} ${sample(places)}`,
             image : 'https://source.unsplash.com/collection/483251',
